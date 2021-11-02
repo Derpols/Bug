9264c77eedd3cc74997356e8100030bba3ce1acc
Derpols <dml96@hotmail.co.uk>
2021-11-02 13:58:03 +0000

    Movement/Look
    Added in movement / look code from a previous project.

---
+++ Buggy/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public float health = 100;
+    public Text healthCountText;
+    int damage = 10;
+    public GameObject player;
+
+    Look _look;
+
+    // Start is called before the first frame update
+
+    public void Start()
+    {
+        
+        Look _look = player.GetComponent<Look>(); // Finds the health script attached to the player's gameobject.
+        //string minutes = ((int)t / 60).ToString(); // Seconds divided by 60 minus decimals.
+        //string seconds = (t % 60).ToString("f0");
+
+        //healthText.text = health + "%"; // Adds health of player.   
+    }
+    public void HealthLoss(float damage) 
+    {
+        health -= damage;
+        Debug.Log("Deduct health count");
+    }
+
+    void Update()
+    {
+        healthCountText.text = "Health:" + health.ToString();
+       
+        if (health < 0) 
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(0);
+            _look.Death(); // Enables the Re-Use of the Mouse Cursor after the player's death.
+        }
+    }
+
+    /*void PlayerDeath() 
+    {
+        if (health < 0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(0);
+            _look.Death(); // Enables the Re-Use of the Mouse Cursor after the player's death.
+        }
+    }
+    */
+}
---
+++ Buggy/Assets/Scripts/Look.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Look : MonoBehaviour
+{
+
+
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+
+    [SerializeField] bool lockCursor = true;
+    [SerializeField] bool playerDeath;
+
+    float cameraPitch = 0.0f;
+    void Start()
+    {
+        if (lockCursor) // Switches the cursor off.
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouseLook();
+    }
+
+    void UpdateMouseLook() 
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        cameraPitch -= mouseDelta.y * mouseSensitivity;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+    }
+
+    public void Death() 
+    {
+        if (playerDeath == true) 
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            Debug.Log("Testing Unlock of Mouse Cursor");
+        }
+    }
+
+    public void ResetMouseView() 
+    {
+        
+    }
+}
